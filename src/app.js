251317e5fc522e9e807e5d6c5cef455146d84485
jlanfell <justin-lanfell@pluralsight.com>
2022-06-24 09:19:51 -0700

    get

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const fs = require("fs")
 const path = require("path")
 const express = require("express")
 const {__} = require("ramda");
+const {render} = require("ejs");
 const app = express()
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "/public")))
+
+app.get("/", () => {
+    render({title: "Index"})
+})
 
