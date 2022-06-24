e921a3cb788a0ffca1c7d21b58442746fed74eb2
jlanfell <justin-lanfell@pluralsight.com>
2022-06-24 09:32:18 -0700

    module 2 - 1

--- src/app.js
+++ src/app.js
@@ -8,8 +8,16 @@ app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "/public")))
 
+const accountData = fs.readFileSync("./src/json/accounts.json", "utf8")
+const accounts = JSON.parse(accountData)
+const userData = fs.readFileSync("./src/json/users.json", "utf8")
+const users = JSON.parse(userData)
+
 app.get("/", (req, res) => {
-    res.render("index", {title: "Index"})
+    res.render("index", {
+        title: "Account Summary",
+        accounts: accounts
+    })
 })
 
 app.listen(3000, () => {
