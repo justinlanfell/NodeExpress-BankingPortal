980a6abcce326f2b0a35e8645efccb840b4050f6
jlanfell <justin-lanfell@pluralsight.com>
2022-06-24 09:47:30 -0700

    module 2 - 3

--- src/app.js
+++ src/app.js
@@ -13,11 +13,27 @@ const accounts = JSON.parse(accountData)
 const userData = fs.readFileSync("./src/json/users.json", "utf8")
 const users = JSON.parse(userData)
 
+
 app.get("/", (req, res) => {
     res.render("index", {
-        title: "Account Summary",
-        accounts: accounts
+        title: "Account Summary", accounts: accounts
     })
+})
+
+app.get("/savings", (req, res) => {
+    res.render("account", {account: accounts.savings})
+})
+
+app.get("/checking", (req, res) => {
+    res.render("account", {account: accounts.checking})
+})
+
+app.get("/credit", (req, res) => {
+    res.render("account", {account: accounts.credit})
+})
+
+app.get("/profile", (req, res) => {
+    res.render("profile", {user: users[0]})
 })
 
 app.listen(3000, () => {
